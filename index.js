f07dfc8c4ccf3abdc09b7184d0f21d1091417fe7
James <jmsn311@gmail.com>
2024-06-06 14:36:38 -0600

    completed lab

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global tutorials variable as data.
+
+
+// How can we "iterate" through individual words in a string?
+// Can we execute an iteration inside an iteration? How?
+// How can we capitalize just the first letter in a word?
+
+function titleCased() {
+  const stuff = tutorials.map(titleCased2)
+  return stuff;
 }
+
+
+function titleCased2(tutorial) {
+  const split = tutorial.split(" "); // split the string into an array of words
+  const capitalized = split.map(word => word.charAt(0).toUpperCase() + word.slice(1)); // capitalize each word
+  const joined = capitalized.join(" "); // join the words back into a string
+  console.log(joined);
+  return joined;
+}
+
+titleCased() //call original function with callback encapsolated
